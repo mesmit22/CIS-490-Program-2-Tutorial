679fd7d76a3a506416c8b7fce85a51068c5bdd45
mesmit22 <mesmit22@gmail.com>
2015-09-21 17:42:19 -0400

    Added Listview, List Adapter, Action bar items

--- Tutorial2/app/src/main/java/com/marksmith/tutorial2/MainActivity.java
+++ Tutorial2/app/src/main/java/com/marksmith/tutorial2/MainActivity.java
@@ -1,23 +1,46 @@
 package com.marksmith.tutorial2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
+    SharedPreferences mSharedPreferences;
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+    ShareActionProvider mShareActionProvider;
 
     @Override
     public void onClick(View v) {
         mainTextView.setText(mainEditText.getText().toString()
                 + "is learning Android Development");
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+        setShareIntent();
     }
 
     @Override
@@ -29,27 +52,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mainButton = (Button)findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
         mainEditText = (EditText)findViewById(R.id.main_edittext);
+        mainListView = (ListView)findViewById(R.id.main_listview);
+        mArrayAdapter = new ArrayAdapter(this,
+                        android.R.layout.simple_list_item_1,
+                        mNameList);
+        mainListView.setAdapter(mArrayAdapter);
+        mainListView.setOnItemClickListener(this);
+        displayWelcome();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+        setShareIntent();
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void setShareIntent() {
+        if (mShareActionProvider != null) {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            mShareActionProvider.setShareIntent(shareIntent);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("omg android", position + ":" + mNameList.get(position));
+    }
+
+    public void displayWelcome() {
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+
+        if (name.length() > 0) {
+            Toast.makeText(this, "Welcome back," + name + "!", Toast.LENGTH_LONG).show();
+        }else {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Hello!");
+            alert.setMessage("What is your name?");
+
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String inputName = input.getText().toString();
+
+                    SharedPreferences.Editor e = mSharedPreferences.edit();
+                    e.putString(PREF_NAME, inputName);
+                    e.commit();
+
+                    Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {}
+            });
+            alert.show();
+        }
     }
 }
